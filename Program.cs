a9dda6dddccda104409d6a209596a050483f7184
kulbhushanmothe <kulbhushan.mothe@gmail.com>
2018-06-27 23:42:43 +0200

    Modified program file with comment

--- Program.cs
+++ Program.cs
@@ -15,41 +15,47 @@ namespace Test
             string sInputLine2 = Console.ReadLine();
 
 
-            string[] sArrayInput1 = sInputLine1.Split(' ');
+            string[] sArrayInput1 = sInputLine1.Trim().Split(' ');
             int iArraySize = 0;
             int iSearchIteam = 0;
 
             if (sArrayInput1.Length != 2)
             {
                 Console.WriteLine("Please enter only two numbers in first line seperated by space.");
+                Console.ReadKey();
                 return;
             }
             if (!int.TryParse(sArrayInput1[0], out iArraySize))
             {
                 Console.WriteLine("Invalid array size entered. Please correct.");
+                Console.ReadKey();
                 return;
             }
             else if (iArraySize < 1 || iArraySize > 100000)
             {
                 Console.WriteLine("Array size must be in the range of 1 to 100000. Please correct.");
+                Console.ReadKey();
                 return;
             }
 
             if (!int.TryParse(sArrayInput1[1], out iSearchIteam))
             {
                 Console.WriteLine("Invalid search element entered. Please enter numeric search element between 1 to 1000000000.");
+                Console.ReadKey();
                 return;
             }
             else if (iSearchIteam < 1 || iSearchIteam > Math.Pow(10, 9))
             {
                 Console.WriteLine("Array size must be in the range of 1 to 1000000000 [power(10,9)]. Please correct.");
+                Console.ReadKey();
                 return;
             }
 
             string[] sArrayElements = sInputLine2.Split(' ');
-            if (sArrayElements.Length < iArraySize)
+            if (sArrayElements.Length != iArraySize)
             {
-                Console.WriteLine("Lesser array elements are provided than entered size.");
+                Console.WriteLine("Array size provided and elementes entered don's match.");
+                Console.ReadKey();
                 return;
             }
 
@@ -60,6 +66,7 @@ namespace Test
                 if (!int.TryParse(sArrayElements[i], out iArrayElements[i]))
                 {
                     Console.WriteLine("Invlid number entered: {0}  Please correct.", sArrayElements[i]);
+                    Console.ReadKey();
                     return;
                 }
                 else if (iSearchIteam == iArrayElements[i])
@@ -68,7 +75,7 @@ namespace Test
                 }
             }
             Console.WriteLine("{0}", index);
-            Console.ReadKey();
+            Console.ReadKey();//Checkin in developer branch
         }
 
         private static int[] ValidateLine1(string sInputLine1)
